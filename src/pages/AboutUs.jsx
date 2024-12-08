d66960c5d43716224cd2be626c7a5161033cc677
JasonL742 <jasonliu741@gmail.com>
2024-12-07 20:13:52 -0500

    added home page

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,14 +1,58 @@
+import { useMediaQuery } from "react-responsive";
+import stats from "../assets/about-us.png";
+import newImage from "../assets/newImage.jpg";
 import "../styles/AboutUs.css";
+import BlurryImageLoader from "../components/BlurryImageLoader";
 
 function AboutUs() {
-    return <DesktopLayout />
+  const isMobile = useMediaQuery({ query: "(max-width: 768px)" });
+
+  return <>{isMobile ? <MobileLayout /> : <DesktopLayout />}</>;
+}
+
+function MobileLayout() {
+  return (
+    <div>
+      <h1>Mobile About Us</h1>
+    </div>
+  );
 }
 
 function DesktopLayout() {
   return (
-    <>
-        <h1>About Us</h1>
-    </>
+    <div>
+      <br />
+      <br />
+      <h1 className="who">Who We Are</h1>
+
+      <div className="who">
+        Welcome to Vintage Watch Co., where time-honored craftsmanship meets
+        modern elegance. We are passionate collectors and enthusiasts of vintage
+        watches, dedicated to curating a remarkable selection of timepieces that
+        tell a story.
+        <br />
+        <br />
+      </div>
+      <div className="bannerBox">
+        <img className="banner" src={stats} alt="About Us Banner" />
+      </div>
+      <br />
+      <br />
+      <h1 className="who">Our Collection</h1>
+
+      <div className="who">
+        Our carefully curated collection features a diverse range of vintage
+        watches from renowned brands, each with its unique charm and character.
+        Whether you are a seasoned collector or new to the world of vintage
+        watches, we strive to offer a selection that caters to every taste and
+        occasion.
+      </div>
+      <br />
+      <br />
+      <div>
+        <BlurryImageLoader src={newImage} alt="Vintage Watch" />
+      </div>
+    </div>
   );
 }
 
