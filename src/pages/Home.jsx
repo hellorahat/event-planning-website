88d305592e016ab42965b0eda7627fee53e02521
Reyaad Raffik <reyaad.raffik57@qmail.cuny.edu>
2024-12-07 20:39:38 -0500

    Adding react-bootstrap components

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,56 @@
 import "../styles/Home.css";
+import Carousel from "react-bootstrap/Carousel";
+import { Link } from "react-router-dom";
+import carouselevent1 from "../assets/carousel-event1.jpg";
+import carouselevent2 from "../assets/carousel-event2.jpg";
+import carouselevent3 from "../assets/carousel-event3.jpg";
 
 function Home() {
-    return <DesktopLayout />
+  return <DesktopLayout />;
 }
 
 function DesktopLayout() {
   return (
     <>
-        <h1>Home</h1>
+      <h1>EVENTFLOW</h1>
+      <Carousel data-bs-theme="dark">
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={carouselevent1}
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <Link to="/catalog" className="watch-caption">
+              Cartier
+            </Link>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={carouselevent2}
+            alt="Second slide"
+          />
+          <Carousel.Caption>
+            <Link to="/catalog" className="watch-caption">
+              Omega
+            </Link>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={carouselevent3}
+            alt="Third slide"
+          />
+          <Carousel.Caption>
+            <Link to="/catalog" className="watch-caption">
+              Rolex
+            </Link>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
     </>
   );
 }
