84223a3795dbb381193692bc1b64db79b81357ad
Rohan <hellorhat@gmail.com>
2024-12-11 09:47:44 -0500

    added success and fail pages

--- src/pages/Cancel.jsx
+++ src/pages/Cancel.jsx
@@ -1,9 +1,13 @@
-// import "../styles/Success.css";
-
+import { Link } from 'react-router-dom';
+import "../styles/Cancel.css"; // Import the CSS file
 
 function Cancel() {
     return (
-        <h1>Cancel</h1>
+        <div className="canceled-container">
+            <h1 className="canceled-title">Order Canceled</h1>
+            <p className="canceled-message">Unfortunately, there was an issue processing your payment. Your order has been canceled.</p>
+            <Link to="/" className="home-link">Go back to Home</Link>
+        </div>
     )
 }
 
--- src/pages/Success.jsx
+++ src/pages/Success.jsx
@@ -1,10 +1,14 @@
-// import "../styles/Success.css";
-
+import { Link } from 'react-router-dom';
+import "../styles/Success.css";
 
 function Success() {
     return (
-        <h1>Success</h1>
-    )
+        <div className="success-container">
+            <h1 className="success-title">Payment Successful!</h1>
+            <p className="success-message">Thank you for your purchase. Your payment has been successfully processed.</p>
+            <Link to="/" className="home-link">Go back to Home</Link>
+        </div>
+    );
 }
 
 export default Success;
