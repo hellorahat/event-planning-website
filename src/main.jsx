39ae651c34fa6f914e0c096534bff7f8d307aff3
Reyaad Raffik <reyaad.raffik57@qmail.cuny.edu>
2024-12-12 00:45:36 -0500

    Imported the appropriate fonts

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,36 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import 'bootstrap/dist/css/bootstrap.css'
-import { BrowserRouter } from 'react-router-dom'
-import { CartProvider } from './utility/CartContext.jsx'
-import { FavoritesProvider } from './utility/FavoritesContext.jsx'
-import { AlertProvider } from './utility/AlertContext.jsx'
-import { UserProvider } from "./utility/UserContext.jsx"
-import AlertComponent from './components/AlertComponent.jsx'
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "bootstrap/dist/css/bootstrap.css";
+import { BrowserRouter } from "react-router-dom";
+import { CartProvider } from "./utility/CartContext.jsx";
+import { FavoritesProvider } from "./utility/FavoritesContext.jsx";
+import { AlertProvider } from "./utility/AlertContext.jsx";
+import { UserProvider } from "./utility/UserContext.jsx";
+import AlertComponent from "./components/AlertComponent.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <BrowserRouter basename="/">
     <>
       <AlertProvider>
         <UserProvider>
           <CartProvider>
             <FavoritesProvider>
-              <link href="https://fonts.googleapis.com/css2?family=Italianno&display=swap" rel="stylesheet" />
+              <link
+                href="https://fonts.googleapis.com/css2?family=Italianno&display=swap"
+                rel="stylesheet"
+              />
+              <link
+                href="https://fonts.googleapis.com/css2?family=Patrick+Hand&display=swap"
+                rel="stylesheet"
+              ></link>
+              <link
+                href="https://fonts.googleapis.com/css2?family=Luckiest+Guy&family=Patrick+Hand&display=swap"
+                rel="stylesheet"
+              ></link>
+              <link
+                href="https://fonts.googleapis.com/css2?family=Ledger&family=Luckiest+Guy&family=Patrick+Hand&display=swap"
+                rel="stylesheet"
+              ></link>
               <App />
               <AlertComponent />
             </FavoritesProvider>
@@ -24,4 +39,4 @@ createRoot(document.getElementById('root')).render(
       </AlertProvider>
     </>
   </BrowserRouter>
-)
+);
